4b9b1ea3cc401f04fdd4d53c578c54dc410103ea
Kevin Hutchins <possessedcow@gmail.com>
2022-11-27 11:54:57 -0800

    Spiced up text box.

--- src/TBDMainScene.ts
+++ src/TBDMainScene.ts
@@ -26,16 +26,31 @@ export class TBDMainScene extends Phaser.Scene {
         this.wrapTypeText.setText(this.naiveWrap ? "Naive word wrapping." : "Wrap-aware word wrapping.")
     }
 
+    drawTextBoxBackground(x: number, y: number, width: number, height: number) {
+        const mult = 3;
+        const outerColor = 0x666666;
+        const innerColor = 0xAAAAAA;
+        const bgColor = 0x222222;
+        this.add.rectangle(x - 2 * mult, y, width + 4 * mult, height, outerColor).setOrigin(0);
+        this.add.rectangle(x, y - 2 * mult, width, height + 4 * mult, outerColor).setOrigin(0);
+        this.add.rectangle(x - mult, y - mult, width + 2 * mult, height + 2 * mult, innerColor).setOrigin(0);
+        this.add.rectangle(x, y, width, height, bgColor).setOrigin(0);
+    }
+
     create() {        
         const width = this.sys.game.scale.gameSize.width;
+        const height=  this.sys.game.scale.gameSize.height;
 
         const inset = 10;
         const text = "This is a very carefully crafted test string.";
+        // this.add.rectangle(10, 35, width - inset * 2, height - 45, 0x000088).setOrigin(0);
+        this.drawTextBoxBackground(10, 35, width - inset * 2, height - 45);
         this.add.bitmapText(inset, inset, 'LegacYs', "Click to animate text.").setOrigin(0).setTint(0xffff00);
         this.wrapTypeText = this.add.bitmapText(width - inset, inset, 'LegacYs', "Naive word wrapping.").setOrigin(1, 0).setTint(0xffff00);
         this.refreshText();
 
-        const animTextObj = new KHAnimatedText(this, inset, 45, 'LegacYs', { naiveWrap: this.naiveWrap, delay: 50 }).setScale(3).setMaxWidth(width - inset * 2);
+        const animTextObj = new KHAnimatedText(this, inset * 2, 45, 'LegacYs', { naiveWrap: this.naiveWrap, delay: 50 }).setScale(3).setMaxWidth(width - inset * 4);
+        animTextObj.setDropShadow(1, 1, 0x0, 1);
 
         this.input.mouse.disableContextMenu();
         
